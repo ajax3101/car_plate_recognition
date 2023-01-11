5ea33ddf844a7d6bfca22c999cc07cc7dec77bba
Denys Morozov <ajax3101@gmail.com>
2023-01-11 17:15:51 +0200

    plate test

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ import cv2
 def open_img(img_path):
     car_plate_img = cv2.imread(img_path)
     car_plate_img = cv2.cvtColor(car_plate_img, cv2.COLOR_BGR2RGB)
+    plt.imshow(car_plate_img)
+    plt.show()
+
+    return car_plate_img
+
+def main():
+    car_plate_img_rgb = open_img(img_path='./plate/1.jpg')
+
+
+if __name__ == '__main__':
+    main()
+
